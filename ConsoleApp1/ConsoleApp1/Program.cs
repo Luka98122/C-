36e6a7b0213bd959af0bbe4d92697e56320245d9
Luka98122 <66061055+Luka98122@users.noreply.github.com>
2024-08-15 14:12:05 +0200

    Quick upload for now, will fix soon

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿public class Account
+{
+    public string Username;
+    public string Email;
+    public string Bio;
+    public List<Account> Followers;
+    public List<Account> Following;
+
+    public Account(string username, string email, string bio)
+    {
+        Username = username;
+        Email = email;
+        Bio = bio;
+        Followers = new List<Account>();
+        Following = new List<Account>();
+    }
+
+    public void PrintFollowers()
+    {
+        Console.WriteLine($"{Username}'s followers:");
+        if (Followers.Count == 0)
+        {
+            Console.WriteLine("No followers.");
+        }
+        else
+        {
+            for (int i = 0;i<Followers.Count;i++)
+            {
+                Console.WriteLine($"- {Followers[i].Username}");
+            }
+        }
+    }
+
+    public void Follow(Account accountToFollow)
+    {
+        if (!Following.Contains(accountToFollow))
+        {
+            Following.Add(accountToFollow);
+            accountToFollow.Followers.Add(this);
+        }
+    }
+    public void PrintFollowing()
+    {
+        Console.WriteLine($"{Username} is following:");
+        if (Following.Count == 0)
+        {
+            Console.WriteLine("Nobody");
+        }
+        else
+        {
+            foreach (var following in Following)
+            {
+                Console.WriteLine($"- {following.Username}");
+            }
+        }
+    }
+}
+
+public class Main2()
+{
+    public static void Main()
+    {
+        var admin = new Account("admin", "admin@example.com", "Administrator");
+
+        var accounts = new List<Account>();
+        for (int i = 1; i < 20; i++)
+        {
+            var account = new Account($"user{i}", $"user{i}@example.com", $"Bio of user{i}");
+            accounts.Add(account);
+        }
+
+        foreach (var account in accounts)
+        {
+            account.Follow(admin);
+        }
+        admin.Follow(accounts[5]);
+        admin.PrintFollowers();
+        admin.PrintFollowing();
+            
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+
+        bool isDangerous = IsDangerous(input);
+        if (isDangerous == true)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+
+        static bool IsDangerous(string s)
+        {
+            int maxLength = 7;
+            int count = 1;
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == s[i - 1])
+                {
+                    count++;
+                    if (count >= maxLength)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+
+            return false;
+        }
+    }
+}
